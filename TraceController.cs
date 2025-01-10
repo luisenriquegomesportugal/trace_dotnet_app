56b4c5b41b34edaa48b5f8c44c4da499a51ee75c
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2025-01-10 01:31:17 -0300

    add local http connection

--- TraceController.cs
+++ TraceController.cs
@@ -9,20 +9,13 @@ namespace TraceApp
     [Route("[controller]")]
     public class TraceController : ControllerBase
     {
-        private readonly HttpClient _httpClient;
-
-        public TraceController()
-        {
-            _httpClient = new HttpClient(new HttpClientHandler());
-        }
-
         [HttpGet]
         public async Task<IActionResult> GetTrace()
         {
-            var externalUrl = "https://jsonplaceholder.typicode.com/posts/1";
-            var response = await _httpClient.GetAsync(externalUrl);
-            response.EnsureSuccessStatusCode();
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync("https://jsonplaceholder.typicode.com/posts/1");
 
+            response.EnsureSuccessStatusCode();
             var responseBody = await response.Content.ReadAsStringAsync();
 
             return Ok(new { responseBody, status = "OK" });
