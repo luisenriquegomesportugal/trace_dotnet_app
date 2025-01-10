6c12116fafef8534af849e3b3b4456d5760aa39b
Luis Enrique Gomes Portugal <luisenriquegomesportugal@gmail.com>
2025-01-10 00:16:00 -0300

    remove proxy to http connection

--- Startup.cs
+++ Startup.cs
@@ -49,6 +49,12 @@ namespace TraceApp
                     {
                         exporter.Endpoint = new Uri(this._otlpEndpoint);
                         exporter.Protocol = OtlpExportProtocol.HttpProtobuf;
+                        exporter.HttpClientFactory = () => new HttpClient(
+                            new HttpClientHandler
+                            {
+                                UseProxy = false,
+                                ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                            });
                     });
                 });
 
